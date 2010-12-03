96831bd87c9b30de09030be1de90afb1d8bf594f
Hiro Asari <asari.ruby@gmail.com>
2010-12-03 11:33:23 -0500

    Avoid popping up a Java app while running on OS X.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 CloudstockDemo::Application.initialize!
+
+require 'java'
+java.lang.System.set_property("java.awt.headless", "true")
