9dd10f25987f2e2b33695e23c2d6243cd23b3550
Hiro Asari <asari.ruby@gmail.com>
2011-01-10 07:23:10 -0500

    forgot a )

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -3,4 +3,4 @@ require 'fileutils'
 
 pdfdir = ["tmp", "pdfs"]
 
-ln_sf(File.join(shared_path, pdfdir), File.join(release_path, pdfdir)
+ln_sf(File.join(shared_path, pdfdir), File.join(release_path, pdfdir))
